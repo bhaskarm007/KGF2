ec77daf567f2ae1214758cb35c8429bc23ca8d94
Lenovo <Lenovo@DESKTOP-UOB7C2O>
2023-06-17 18:43:08 +0530

    keyboard operation Enter key operate

---
+++ src/webelements/EnterButton.java
@@ -0,0 +1,16 @@
+package webelements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class EnterButton {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.actitime.com/login.do");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.id("loginButton")).sendKeys(Keys.ENTER);
+	}
+}
