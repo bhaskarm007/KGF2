489d8a9ae6bc2e87b5ac21f3535ab4481bee9722
Lenovo <Lenovo@DESKTOP-UOB7C2O>
2023-06-17 18:48:07 +0530

    getting illegalArgumentExpection

---
+++ src/webelements/IllegalArgumantEx.java
@@ -0,0 +1,14 @@
+package webelements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class IllegalArgumantEx {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.actitime.com/login.do");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys(null);
+	}
+}
