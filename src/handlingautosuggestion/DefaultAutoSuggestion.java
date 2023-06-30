4c38529c8af9985448f909d5cdb11082899a9f70
Lenovo <Lenovo@DESKTOP-UOB7C2O>
2023-06-30 18:23:23 +0530

    Verify Default Auto Suggestions

---
+++ src/handlingautosuggestion/DefaultAutoSuggestion.java
@@ -0,0 +1,28 @@
+package handlingautosuggestion;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class DeffaultAutoSuggestion {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+		
+		driver.switchTo().activeElement().click();
+    
+		List<WebElement> deffaultAutosuggestionList = driver.findElements(By.xpath("//li[@class='sbct' and not (@id)]"));
+		
+          for (WebElement ele : deffaultAutosuggestionList) {
+			System.out.println(ele.getText());
+		}
+          deffaultAutosuggestionList.get(2).click();
+		
+		//driver.quit();
+	}
+}
