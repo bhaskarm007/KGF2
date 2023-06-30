e5bc4478fe2ea413459cf3c2de2fb70a67013240
Lenovo <Lenovo@DESKTOP-UOB7C2O>
2023-06-30 18:28:31 +0530

    verify Click on 3rd AutoSuggestions name

---
+++ src/handlingautosuggestion/ThirdOneClick.java
@@ -0,0 +1,24 @@
+package handlingautosuggestion;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class ThirdOneClick {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		
+		driver.manage().window().maximize();
+		
+        driver.findElement(By.name("q")).sendKeys("Selenium");
+		
+        Thread.sleep(5000);
+		List<WebElement> autosuggestionList = driver.findElements(By.xpath("//li[@class='sbct' and not (@id)]"));
+		autosuggestionList.get(2).click();
+		
+		//driver.quit();
+	}
+}
