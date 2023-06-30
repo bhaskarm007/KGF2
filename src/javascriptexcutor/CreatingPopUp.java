92076ad327f6ab5f76d513c7d2c4e69ea825a9da
Lenovo <Lenovo@DESKTOP-UOB7C2O>
2023-06-30 18:32:28 +0530

    Program to understand how to use executeScript method with jsCode.

--- src/javascriptexcutor/CreatingPopUp.java
+++ src/javascriptexcutor/CreatingPopUp.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 //Program to understand how to use executeScript method with jsCode.
 
 public class CreatingPopUp {
