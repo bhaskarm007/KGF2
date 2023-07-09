23fba0a48ae23b19179bc0f87e4f9b8189801943
Lenovo <Lenovo@DESKTOP-UOB7C2O>
2023-07-09 11:47:53 +0530

    Usage of implicit Wait

---
+++ src/synchronization/ImplicitWait.java
@@ -0,0 +1,30 @@
+package synchronization;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class ImplicitWait {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.actiTime.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+//Before selenium 4.0 
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		
+		driver.findElement(By.id("loginButton")).click();
+		
+		driver.findElement(By.id("logoutLink")).click();
+		driver.quit();
+	}
+}
+
+// Today onwards remove all Thread.sleep() 
+// Use the Implicit Wait
