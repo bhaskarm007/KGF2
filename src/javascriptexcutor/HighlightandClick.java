c6a9bfc3eb451de4aed1ab41da78c91dd014ee64
Lenovo <Lenovo@DESKTOP-UOB7C2O>
2023-07-09 11:21:36 +0530

    Library method to highlighted the target WebElement and click on it

--- src/javascriptexcutor/HighlightandClick.java
+++ src/javascriptexcutor/HighlightandClick.java
@@ -5,7 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+// Library method to highlighted the target WebElement and click on it
 public class HighlightandClick {
 public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
@@ -14,7 +14,9 @@ public static void main(String[] args) {
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	driver.get("https://www.naukri.com/");
 	
+	// Fetching LibraryClass to executing
 	LibraryClass.higlightedAndClick(driver, driver.findElement(By.cssSelector("a[id='login_Layer']")));
+	
 	driver.findElement(By.cssSelector("input[placeholder='Enter your active Email ID / Username']")).sendKeys("nameishemanth@rediffmail.com");
 	driver.findElement(By.cssSelector("input[placeholder='Enter your password']")).sendKeys("Testing@123");
 	LibraryClass.higlightedAndClick(driver, driver.findElement(By.xpath("//button[text()='Login']")));
