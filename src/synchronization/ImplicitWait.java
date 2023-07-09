c80e475450b1fc2bbfc74f1a57b3639299eaa55d
Lenovo <Lenovo@DESKTOP-UOB7C2O>
2023-07-09 11:48:34 +0530

    Usage of implicitly Wait

--- src/synchronization/ImplicitWait.java
+++ src/synchronization/ImplicitWait.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class ImplicitWait {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://demo.actiTime.com");
@@ -21,6 +21,8 @@ public class ImplicitWait {
 		
 		driver.findElement(By.id("loginButton")).click();
 		
+		// Blind Wait
+		//Thread.sleep(5000);
 		driver.findElement(By.id("logoutLink")).click();
 		driver.quit();
 	}
