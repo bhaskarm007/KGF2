8ee3f77b851917eedc6cb94018bbb686c28f0b6f
bhaskarm007 <bhaskarchindi@gmail.com>
2023-07-21 18:38:18 +0530

    get Total number of Row and Cells

---
+++ src/datadriventesting/excel/GetTotalRowsCells.java
@@ -0,0 +1,38 @@
+package datadriventesting.excel;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+// Script to get Total number of Row and Cells
+public class GetTotalRowsCells {
+
+	public static void main(String[] args) throws IOException {
+		
+		String filePath = "./resources/testdata.xlsx";
+		
+		FileInputStream fin = new FileInputStream(filePath);
+		
+		Workbook workbook = WorkbookFactory.create(fin);
+		
+		int rowCount = workbook.getSheet("Multiple").getPhysicalNumberOfRows();
+		int cellCount = workbook.getSheet("Multiple").getRow(0).getPhysicalNumberOfCells();
+		
+		System.out.println("rowCount="+rowCount);
+		System.out.println("cellCount="+cellCount);
+	}
+}
+
+// Excel data
+/*
+username	password
+admin		manager
+trainee		trainee
+bhaskar		bhaskar123
+bharath		bharath123
+mahi		mahi007
+
+*/
