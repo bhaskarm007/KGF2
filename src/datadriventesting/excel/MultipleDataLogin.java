3d758e512c2ced1d4e7042ade5c7453726641fe8
bhaskarm007 <bhaskarchindi@gmail.com>
2023-07-21 18:47:29 +0530

    login multiple data, different username and password

---
+++ src/datadriventesting/excel/MultipleDataLogin.java
@@ -0,0 +1,63 @@
+package datadriventesting.excel;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+// Script to login multiple data
+
+public class MultipleDataLogin {
+	
+public static void main(String[] args) throws IOException {
+	
+	String filePath = "./resources/testdata.xlsx";
+	
+	FileInputStream fin = new FileInputStream(filePath);
+	
+	Workbook workbook = WorkbookFactory.create(fin);
+	
+	Sheet sheet = workbook.getSheet("Multiple");
+	
+	int rowCount = sheet.getPhysicalNumberOfRows();
+	int cellCount = sheet .getRow(0).getPhysicalNumberOfCells();
+	
+	System.out.println("rowCount="+rowCount);
+	System.out.println("cellCount="+cellCount);
+	
+	String[][] sarr = new String[rowCount][cellCount];
+	
+	// Iterate the rows
+	for(int i=0;i<=rowCount-1;i++) {
+		// Iterate the cells
+		for(int j=0;j<=cellCount-1;j++) {
+			sarr[i][j] = sheet.getRow(i).getCell(j).toString();
+		}
+	}
+	
+	for(int i=0;i<=sarr.length-1;i++) {
+		System.setProperty("Webdriver.chrome.driver","./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demo.actitime.com");
+		driver.findElement(By.id("username")).sendKeys(sarr[i][0]);
+		driver.findElement(By.name("pwd")).sendKeys(sarr[i][1]);
+		}
+    }
+}
+//Excel data
+/*
+username	password
+admin		manager
+trainee		trainee
+bhaskar		bhaskar123
+bharath		bharath123
+mahi		mahi007
+
+*/
