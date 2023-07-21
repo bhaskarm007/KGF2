636b7eb0e13e15d4b5ed756aa067fc704c9919dd
bhaskarm007 <bhaskarchindi@gmail.com>
2023-07-21 17:56:26 +0530

    Read from Excel Sheet, and Converting any Data into String

---
+++ src/datadriventesting/excel/ConvertingAnyDataString.java
@@ -0,0 +1,55 @@
+package datadriventesting.excel;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+// Data Read from Excel Sheet, and Converting any Data into String
+
+public class ConvertingAnyDataString {
+
+	public static void main(String[] args) throws IOException {
+	
+		String filePath = "./resources/testdata.xlt";
+		
+		FileInputStream fin = new FileInputStream(filePath);
+		
+		Workbook workbook = WorkbookFactory.create(fin);
+		
+		String name = workbook.getSheet("TC002").getRow(1).getCell(0).toString();
+		String age = workbook.getSheet("TC002").getRow(1).getCell(1).toString();
+		String passed = workbook.getSheet("TC002").getRow(1).getCell(2).toString();
+		String date = workbook.getSheet("TC002").getRow(1).getCell(3).toString();
+		
+		System.out.println("name="+name);
+		System.out.println("age="+age);
+		System.out.println("passed="+passed);
+		System.out.println("date="+date);
+		
+		System.out.println("---------Afteer Parsing---------");
+		
+		int myAge= (int) Double.parseDouble(age);
+		boolean pass = Boolean.parseBoolean(passed);
+		System.out.println("myAge="+myAge);
+		System.out.println("pass="+pass);
+		
+		System.out.println("-----After Converting to String----------");
+		
+		String myRealAge = Integer.toString(myAge);
+		String p= Boolean.toString(pass);
+		
+		System.out.println("myRealAge="+myRealAge);
+		System.out.println("p="+p);
+}
+}
+
+//Excel sheet data
+//Saved this into (97-2003) extension .xlt
+/*
+String Data||Number||Boolean||Date
+  Mahi     || 7    ||TRUE   ||17-06-1998
+
+*/
