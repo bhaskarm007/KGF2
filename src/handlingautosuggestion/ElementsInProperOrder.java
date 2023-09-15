f09a3e46c304267bf97b1d649aa622c07376c525
bhaskarm007 <bhaskarchindi@gmail.com>
2023-09-15 12:12:36 +0530

    understand webPage Elements in ProperOrder showing

--- src/handlingautosuggestion/ElementsInProperOrder.java
+++ src/handlingautosuggestion/ElementsInProperOrder.java
@@ -7,8 +7,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+// Script to understand webPage Elements in ProperOrder showing
 public class ElementsInProperOrder {
+	
 	public static void main(String[] args) throws InterruptedException {
+		
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
