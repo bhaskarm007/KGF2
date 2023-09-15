8e32c654334f7e9b41376af871477ae9feff1799
bhaskarm007 <bhaskarchindi@gmail.com>
2023-09-15 11:53:23 +0530

    understand parallel execution in @DataProvider

---
+++ src/testngtopic/multiple/ParallelExecution.java
@@ -0,0 +1,34 @@
+package testngtopic.multiple;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+//Script to understand parallel execution in @DataProvider
+
+public class ParallelExecution {
+
+	@DataProvider(name= "credentials",parallel=true)
+	public String[][] testData() {
+		String[][] sarr= {{"admin", "manager"}, {"trainee","trainee"}, {"mahibh","mahibh007"}};
+	return sarr;
+	}
+	
+	@Test(dataProvider = "credentials")
+	public void testMultipleLogins(String userData, String passwordData) {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demo.actitime.com");
+		
+		driver.findElement(By.id("username")).sendKeys(userData);
+		driver.findElement(By.name("pwd")).sendKeys(passwordData);
+		
+	}
+	
+}
