ff9ca53a7fdcc15b255139080fb8da8406c005bc
bhaskarm007 <bhaskarchindi@gmail.com>
2023-09-16 17:09:36 +0530

    understand @BeforeTest and @AfterTests

---
+++ src/testngtopic/xmlconfig/TC022.java
@@ -0,0 +1,11 @@
+package testngtopic.xmlconfig;
+
+import org.testng.annotations.Test;
+
+public class TC022 extends BaseTest1{
+
+	@Test
+	public void addFamily1() {
+		System.out.println("Running addFamily1 testCase");
+	}
+}
---
+++ src/testngtopic/xmlconfig/TC023.java
@@ -0,0 +1,11 @@
+package testngtopic.xmlconfig;
+
+import org.testng.annotations.Test;
+
+public class TC023 extends BaseTest1{
+
+	@Test
+	public void addFamily2() {
+		System.out.println("Running addFamily2 testCase");
+	}
+}
