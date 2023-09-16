87d7572bb6a154b79019a936cdef95120d50bb14
bhaskarm007 <bhaskarchindi@gmail.com>
2023-09-16 17:04:00 +0530

    understand what will happen if we have 2 @test

---
+++ src/testngtopic/xmlconfig/TC013.java
@@ -0,0 +1,29 @@
+package testngtopic.xmlconfig;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+// Script to understand what will happen if we have 2 @test
+public class TC013 {
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Running beforeMethod");
+		}
+	
+	@Test
+	public void testCase1() {
+		System.out.println("Running testCase1");
+	}
+	
+	@Test
+	public void testCase2() {
+		System.out.println("Running testCase2");
+	}
+	
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("Running afterMethod");
+     }
+}
