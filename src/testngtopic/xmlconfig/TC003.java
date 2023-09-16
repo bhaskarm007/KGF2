f7c335387cb2e171f4361352d0cfdbbf2a504db7
bhaskarm007 <bhaskarchindi@gmail.com>
2023-09-16 16:56:28 +0530

    understand TestNG class without using XML file/not created then TestNG will create Default XML file.

---
+++ src/testngtopic/xmlconfig/TC003.java
@@ -0,0 +1,13 @@
+package testngtopic.xmlconfig;
+
+import org.testng.annotations.Test;
+
+// Script to understand TestNG class without using XML file/not created
+//    then TestNG will create Default XML file.
+public class TC003 {
+
+	@Test
+	public void tesdAddToKart() {
+		System.out.println("Running testAddToKart Test Case");
+	}
+}
