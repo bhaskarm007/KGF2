92bd6808bc74e9ce958387d6ff160a8ab32665b0
bhaskarm007 <bhaskarchindi@gmail.com>
2023-09-16 16:54:57 +0530

    understand the .xml file Batch Execution(TC001 && TC002)

---
+++ src/testngtopic/xmlconfig/TC001.java
@@ -0,0 +1,12 @@
+package testngtopic.xmlconfig;
+
+import org.testng.annotations.Test;
+
+// Script to understand the .xml file Batch Execution(TC001 && TC002)
+public class TC001 {
+
+	@Test
+	public void testAddFriend() {
+		System.out.println("Running Add Friend Test Case");
+	}
+}
---
+++ src/testngtopic/xmlconfig/TC002.java
@@ -0,0 +1,15 @@
+package testngtopic.xmlconfig;
+
+import org.testng.annotations.Test;
+
+//Script to understand the .xml file Batch Execution(TC001 && TC002)
+public class TC002 {
+
+	@Test
+	public void testProfile() {
+		System.out.println("Running Profile Test Case");
+	}
+}
+
+// comment in xml file
+// ctrl+shift+/(forward slash)
